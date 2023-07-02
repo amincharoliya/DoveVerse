2313392bbb52dce386de4c8b09f4bf055e6d934a
Amin Charoliya <amincharoliya@gmail.com>
2023-07-02 15:14:10 +0530

    Remove dummy UserState component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 
-import UserState from '../components/UserState';
-
 export default function Home() {
 	const { status, data: session } = useSession();
 	const router = useRouter();
@@ -18,8 +16,7 @@ export default function Home() {
 
 	return (
 		<main className="flex flex-col font-inter font-medium text-slate-900 dark:text-white bg-slate-100 dark:bg-slate-900 min-h-screen">
-			<UserState />
-			{/* <Tweet /> */}
+			Homepage
 		</main>
 	);
 }
